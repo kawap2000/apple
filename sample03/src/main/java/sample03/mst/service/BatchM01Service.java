1c065ad08e514e9ef0d7bcaabe7e51c4a783e4d3
hiroshi-kawashima-v <hiroshi-kawashima-v@hiroshi-kawas01.ad.exa-corp.co.jp>
2022-08-30 10:00:22 +0900

    devAのバッチ処理０１をコミット

--- sample03/src/main/java/sample03/mst/service/BatchM01Service.java
+++ sample03/src/main/java/sample03/mst/service/BatchM01Service.java
@@ -2,6 +2,11 @@ package sample03.mst.service;
 
 public class BatchM01Service {
 	public String exec() {
+		String msg1 = "develop A のバッチ処理";
+		String msg2 = "最初のバッチ処理開始";
+		System.out.println(msg1);
+		System.out.println(msg2);
+		
 		return "OK";
 	}
 
