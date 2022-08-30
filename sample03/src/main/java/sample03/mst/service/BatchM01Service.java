b6a119a62d55d229db85ab8b195b7a17f09996b2
hiroshi-kawashima-v <hiroshi-kawashima-v@hiroshi-kawas01.ad.exa-corp.co.jp>
2022-08-30 10:28:09 +0900

    develop B 2回目のバッチ実装

--- sample03/src/main/java/sample03/mst/service/BatchM01Service.java
+++ sample03/src/main/java/sample03/mst/service/BatchM01Service.java
@@ -2,6 +2,10 @@ package sample03.mst.service;
 
 public class BatchM01Service {
 	public String exec() {
+		String msg1 = "develop B のバッチ処理";
+		String msg2 = "2回目のバッチ処理開始";
+		System.out.println(msg1);
+		System.out.println(msg2);
 		return "OK";
 	}
 
