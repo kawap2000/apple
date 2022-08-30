04b7ba4a79e2ea3c272751abb67bee4239c35d24
hiroshi-kawashima-v <hiroshi-kawashima-v@hiroshi-kawas01.ad.exa-corp.co.jp>
2022-08-30 10:44:42 +0900

    BatchM01の処理を削除、BatchM03へ移動

--- sample03/src/main/java/sample03/mst/service/BatchM01Service.java
+++ sample03/src/main/java/sample03/mst/service/BatchM01Service.java
@@ -2,10 +2,6 @@ package sample03.mst.service;
 
 public class BatchM01Service {
 	public String exec() {
-		String msg1 = "develop C のバッチ処理";
-		String msg2 = "最後のバッチ処理開始";
-		System.out.println(msg1);
-		System.out.println(msg2);
 		return "OK";
 	}
 
--- sample03/src/main/java/sample03/mst/service/BatchM03Service.java
+++ sample03/src/main/java/sample03/mst/service/BatchM03Service.java
@@ -2,6 +2,10 @@ package sample03.mst.service;
 
 public class BatchM03Service {
 	public String exec() {
+		String msg1 = "develop C のバッチ処理";
+		String msg2 = "最後のバッチ処理開始";
+		System.out.println(msg1);
+		System.out.println(msg2);
 		return "OK";
 	}
 
