fb0151a0817f1043dad3167b5f382edeacec7085
hiroshi-kawashima-v <hiroshi-kawashima-v@hiroshi-kawas01.ad.exa-corp.co.jp>
2022-09-02 11:44:01 +0900

    ローカルをリモートに

--- sample03/src/main/java/sample03/bat/service/SampleService.java
+++ sample03/src/main/java/sample03/bat/service/SampleService.java
@@ -6,24 +6,25 @@ import java.util.List;
 import sample03.mst.service.BatchM01Service;
 import sample03.mst.service.BatchM02Service;
 import sample03.mst.service.BatchM03Service;
+import sample03.mst.service.BatchM04Service;
 
 public class SampleService {
 
 	private BatchM01Service batch01 = new BatchM01Service();
 	private BatchM02Service batch02 = new BatchM02Service();
 	private BatchM03Service batch03 = new BatchM03Service();
+	private BatchM04Service batch04 = new BatchM04Service();
 
 	public List<String> search() {
-		String[] TABLE = {
-				"abcde",
-				"fghij",
-				"klmno",
-				"pqrst",
-				"uvwxy"
+		String[] TABLE01 = {
+				"abcd01",
+				"abcd02",
+				"abcd03",
+				"abcd04",
+				"abcd05",
 		};
-		List<String> result = null;
-		result = new ArrayList<String>();
-		for (String string : TABLE) {
+		List<String> result = new ArrayList<String>();
+		for (String string : TABLE01) {
 			result.add(string);
 			
 		}
@@ -41,5 +42,9 @@ public class SampleService {
 
 	public String executeFinish() {
 		return batch03.exec();
+	}
+
+	public String executeExtFinish() {
+		return batch04.exec();
 	}
 }
